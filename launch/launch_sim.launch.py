84a3a61d09cb2c65ddf41e7b99a8a9e1863cd151
Tran Dinh Thuong <thuong111104@gmail.com>
2025-08-07 19:25:06 +0700

    da mo phong duoc gazebo

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,77 @@
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription , DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+"""
+gazebo_launch = IncludeLaunchDescription(
+    PathJoinSubstitution([
+        FindPackageShare('ros_gz_sim'),
+        'launch',
+        'gz_sim.launch.py'
+    ]),
+    launch_arguments={
+        'gz_args': ['-r empty.sdf'],
+        'gui': LaunchConfiguration('gui'),
+        'on_exit_shutdown': 'true'
+    }.items()
+)
+"""
+
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='mobile_robot' #<--- CHANGE ME
+    gui_arg = DeclareLaunchArgument(
+            name='gui',
+            default_value='true',
+        )
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    # gazebo = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+    #          )
+    gazebo_launch = IncludeLaunchDescription(
+    PathJoinSubstitution([
+        FindPackageShare('ros_gz_sim'),
+        'launch',
+        'gz_sim.launch.py'
+    ]),
+    launch_arguments={
+        'gz_args': ['-r empty.sdf'],
+        'gui': LaunchConfiguration('gui'),
+        'on_exit_shutdown': 'true'
+    }.items()
+)
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot',
+                                   '-z', '0.2'],
+                        output='screen')
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        gui_arg,
+        rsp,
+        gazebo_launch,
+        spawn_entity,
+    ])
---
+++ launch/rviz2.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    robot_description_content = Command([
+        'xacro ',
+        PathJoinSubstitution([
+            FindPackageShare('mobile_robot'),
+            'xacro',
+            'robot.xacro'
+        ])
+    ])
+
+    return LaunchDescription([
+        # robot_state_publisher
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description_content}]
+        ),
+
+        # joint_state_publisher_gui (điều khiển joint bằng slider GUI)
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+
+        # RViz (tùy chọn)
+        # rviz2 with config
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=[
+                '-d',
+                PathJoinSubstitution([
+                    FindPackageShare('mobile_robot'),
+                    'rviz',
+                    'rviz2.rviz'
+                ])
+            ],
+            output='screen'
+        )
+    ])
